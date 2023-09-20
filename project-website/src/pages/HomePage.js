8d731f9d0cfb3c46cc23e88a5db6733038debec1
Peter Cesmegi <pdc1601@rit.edu>
2023-09-20 18:31:00 -0400

    Added content to site

--- project-website/src/pages/HomePage.js
+++ project-website/src/pages/HomePage.js
@@ -5,12 +5,30 @@ function HomePage() {
   return (
     <div className="HomePage">
         <header className="HomePage-header">
-            <HomePageSection name="Team" content="..."/>
-            <HomePageSection name="Project" content="..."/>
-            <HomePageSection name="Sponsor" content="..."/>
-            <HomePageSection name="Coach" content="..."/>
-            <HomePageSection name="Team Members" content="..."/>
-            <HomePageSection name="Synopsis" content="..."/>
+            {/* Over view */}
+            <HomePageSection name="Team" content="Spooky Frogs"/>
+            <HomePageSection name="Project" content="LenelS2 - Multi-Cloud Monitoring Dashboard"/>
+            <HomePageSection name="Sponsor" content="LenelS2"/>
+            <HomePageSection name="Coach" content="Warren Tessier"/>
+
+            {/* Team Members */}
+            <HomePageSection name="Team Members" content="Andreas Leonard-Calcano --- Tech Lead, DevOps, System Architect"/>
+            <HomePageSection name="" content="Dominique Smith-Rodrique --- Design Specialist (Ui/UX)"/>
+            <HomePageSection name="" content="Ethan Hower --- Documentation Master"/>
+            <HomePageSection name="" content="Peter Cesmegi --- Frontend Lead, Database Master"/>
+            <HomePageSection name="" content="Skyler Herman --- Sponsor Communication Lead, Scribe, Scrum Master/Project Manager"/>
+            
+            {/* Synopsis */}
+            <HomePageSection name="Synopsis" content="The goal of this project is to create a ''single pane of glass'' to function as a monitoring system for the multiple cloud platforms used by LenelS2’s Elements, OnGuard Cloud, and Brokerbay programs."/>
+
+            {/* Time Tracking */}
+            <HomePageSection name="Time Tracking" content=""/>
+            <img filepath="week3/PeterCesmegi-Week-Time.pdf" alt="Week Time Tracking (Peter Cesmegi)" />
+
+            {/* Future Deliverables */}
+            <HomePageSection name="Future Deliverables" content="..."/>
+            
+
         </header>
     </div>
   );
